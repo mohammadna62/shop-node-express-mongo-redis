ec873a404da38aee03e2638e9672fdc44dba1161
Mohammad Naghavi Olyaei <mohammad.naghavi@gmail.com>
2026-06-07 12:48:36 +0330

    feat: develop User model

---
+++ models/Users.js
@@ -0,0 +1,51 @@
+const { default: mongoose } = require("mongoose");
+
+const addressSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  postalCode: {
+    type: String,
+    required: true,
+  },
+  location: {
+    lat: {
+      type: Number,
+      required: true,
+    },
+    lng: {
+      type: Number,
+      required: true,
+    },
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  cityId: {
+    type: Number,
+    required: true,
+  },
+});
+const userSchema = new mongoose.Schema(
+  {
+    phone: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    role: {
+      type: [String],
+      enum: ["ADMIN", "USER", "SELLER"],
+      default: ["USER"],
+    },
+    addresses: [addressSchema],
+  },
+  {
+    timestamps: true,
+  },
+);
+
+
+module.exports = mongoose.model("User",userSchema)
