faefe4d9c611d37094684f89a950b1098a9e4af2
Mohammad Naghavi Olyaei <mohammad.naghavi@gmail.com>
2026-06-08 12:24:44 +0330

    feat: send OTP code to user via SMS - part 1

--- controllers/v1/auth.js
+++ controllers/v1/auth.js
@@ -1,5 +1,49 @@
+const redis = require("./../../redis");
+const { errorResponse, successResponse } = require("../../helpers/responses");
+const Ban = require("./../../models/Ban");
+
+//* Start Helper Functions
+
+function getOtpRedisPattern(phone) {
+  return `otp:${phone}`;
+}
+async function getOtpDetails(phone) {
+  const otp = await redis.get(getOtpRedisPattern(phone));
+  if (!otp) {
+    return {
+      expired: true,
+      remainingTime: 0,
+    };
+  }
+  const remainingTime = await redis.ttl(getOtpRedisPattern(phone)); //Second
+  const minutes = Math.floor(remainingTime / 60);
+  const seconds = remainingTime % 60;
+  const formattedTime = `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+  return {
+    expired: false,
+    remainingTime: formattedTime,
+  };
+}
+
+//* Finish Helper Functions
+
 exports.send = async (req, res, next) => {
-  //Code
+  try {
+    const { phone } = req.body;
+    const isBanned = await Ban.findOne({ phone });
+    if (isBanned) {
+      return errorResponse(res, 403, "This Phone Number has been banned ");
+    }
+    //*Validation
+    const { expired, remainingTime } = await getOtpDetails(phone);
+    if (!expired) {
+      return successResponse(res, 200, {
+        message: `OTP Already Send Please Tray Again After ${remainingTime}`,
+      });
+    }
+  } catch (err) {
+    next(err);
+  }
 };
 
 exports.verify = async (req, res, next) => {
--- helpers/responses.js
+++ helpers/responses.js
@@ -1,9 +1,13 @@
-const errorResponse = (res, statusCode,message, data) => {
-  return res.status(statusCode).json({ status: statusCode, success: flase ,error:message, data});
+const errorResponse = (res, statusCode, message, data) => {
+  return res
+    .status(statusCode)
+    .json({ status: statusCode, success: flase, error: message, data });
 };
 
 const successResponse = (res, statusCode = 200, data) => {
-  return res.status(statusCode).json({ status: statusCode, success: true,data });
+  return res
+    .status(statusCode)
+    .json({ status: statusCode, success: true, data });
 };
 
 module.exports = { errorResponse, successResponse };
