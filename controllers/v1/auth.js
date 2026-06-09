db5dc7b368b83bd72c6b5da1216ba2fcb8dd8eea
Mohammad Naghavi Olyaei <mohammad.naghavi@gmail.com>
2026-06-09 10:25:13 +0330

    feat: send OTP code to user via SMS - part 2

--- controllers/v1/auth.js
+++ controllers/v1/auth.js
@@ -1,6 +1,8 @@
 const redis = require("./../../redis");
 const { errorResponse, successResponse } = require("../../helpers/responses");
 const Ban = require("./../../models/Ban");
+const bcrypt = require("bcrypt");
+const { sendSms } = require("../../services/otp");
 
 //* Start Helper Functions
 
@@ -24,12 +26,23 @@ async function getOtpDetails(phone) {
     remainingTime: formattedTime,
   };
 }
-
+const generateOtp = async (phone, length = 4, expireTime = 1) => {
+  const digit = `0123456789`;
+  let otp = "";
+  for (let i = 0; i < length; i++) {
+    otp += digit[Math.random() * digit.length];
+  }
+  const hashedOtp = await bcrypt.hash(otp, 12);
+  await redis.set(getOtpRedisPattern(phone), hashedOtp, "EX", expireTime * 60);
+  return otp;
+};
 //* Finish Helper Functions
 
 exports.send = async (req, res, next) => {
   try {
     const { phone } = req.body;
+    console.log("phone");
+    return res.json("ok")
     const isBanned = await Ban.findOne({ phone });
     if (isBanned) {
       return errorResponse(res, 403, "This Phone Number has been banned ");
@@ -41,6 +54,9 @@ exports.send = async (req, res, next) => {
         message: `OTP Already Send Please Tray Again After ${remainingTime}`,
       });
     }
+    const otp = generateOtp(phone);
+    await sendSms(phone, otp);
+    return successResponse(res, 200, { message: "OTP Send Successfully" });
   } catch (err) {
     next(err);
   }
---
+++ services/otp.js
@@ -0,0 +1,44 @@
+
+require("dotenv")
+exports.sendSms = async(phone,opt)=>{
+    console.log("sendSMS");
+    
+ try {
+    request.post(
+      {
+        url: "http://ippanel.com/api/select",
+        body: {
+          op: "pattern",
+          user: process.env.SMS_UNAME,
+          pass: process.env.SMS_PASSWORD,
+          fromNum: process.env.SMS_FROM,
+          toNum: phone,
+          patternCode: process.env.VERIFY_PATTERN_CODE,
+          inputData: [{ "verification-code": opt }],
+        },
+        json: true,
+      },
+      async function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+          //YOU‌ CAN‌ CHECK‌ THE‌ RESPONSE‌ AND SEE‌ ERROR‌ OR‌ SUCCESS‌ MESSAGE
+
+          if (
+            typeof response.body !== "number" &&
+            Number(response.body[0]) !== 0
+          ) {
+            return res.status(500).json({ message: response.body[1] });
+          }
+          await otpModel.create({ phone, code, expireAt });
+          return res
+            .status(201)
+            .json({ message: "OTP Code Send Successfully" });
+        } else {
+          console.log("whatever you want");
+        }
+      },
+    );
+  } catch (err) {
+    console.log("Error ->",err);
+    
+  }
+}
