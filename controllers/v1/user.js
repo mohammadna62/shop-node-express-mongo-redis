b29f147d6f0ea4eadbbf390e4ae2bda5ffa4a4c3
Mohammad Naghavi Olyaei <mohammad.naghavi@gmail.com>
2026-06-13 12:52:12 +0330

    feat: add address to user's address list

--- controllers/v1/user.js
+++ controllers/v1/user.js
@@ -1,6 +1,7 @@
 const { errorResponse, successResponse } = require("../../helpers/responses");
 const Ban = require("../../models/Ban");
 const User = require("../../models/Users");
+const cities = require("./../../cities/cities.json");
 
 exports.banUser = async (req, res, next) => {
   try {
@@ -23,6 +24,48 @@ exports.banUser = async (req, res, next) => {
     return successResponse(res, 200, {
       user: deletedUser,
       message: "User banned successfully, user and posts removed",
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.createAddress = async (req, res, next) => {
+  try {
+    const user = req.user;
+    const { name, postalCode, location, address, cityId } = req.body;
+
+    //* Validation
+
+    const city = cities.find((city) => +city.id === +cityId);
+
+    if (!city) {
+      return errorResponse(res, 409, "City is not valid !!");
+    }
+
+    const addressObject = {
+      name,
+      postalCode,
+      location,
+      address,
+      cityId,
+    };
+
+    const updatedUser = await User.findByIdAndUpdate(
+      user._id,
+      {
+        $push: {
+          addresses: addressObject,
+        },
+      },
+      {
+        new: true,
+      },
+    );
+
+    return successResponse(res, 201, {
+      user: updatedUser,
+      message: "Address created successfully :))",
     });
   } catch (err) {
     next(err);
--- routes/v1/user.js
+++ routes/v1/user.js
@@ -1,11 +1,12 @@
 const express = require("express")
-const { banUser } = require("../../controllers/v1/user")
+const { banUser,createAddress } = require("../../controllers/v1/user")
 const {auth} = require("./../../middlewares/auth")
 const roleGuard = require("../../middlewares/roleGuard")
 
 const router = express.Router()
 
 router.route('/ban/:userId').post(auth,roleGuard("ADMIN"),banUser)
+router.route('/me/addresses').post(auth,createAddress)
 
 
 
