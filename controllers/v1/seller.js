51ae43c74135525cf43a5265819a90fb90ab20c1
Mohammad Naghavi Olyaei <mohammad.naghavi@gmail.com>
2026-06-16 12:43:54 +0330

    feat: update store information by seller

--- controllers/v1/seller.js
+++ controllers/v1/seller.js
@@ -37,3 +37,43 @@ exports.create = async (req, res, next) => {
     next(err);
   }
 };
+
+exports.update = async (req, res, next) => {
+  try {
+    const { name, contactDetails, cityId } = req.body;
+    const user = req.user;
+
+    await updateSellerValidator.validate(req.body, { abortEarly: false });
+
+    const existingSeller = await Seller.findOne({ user: user._id });
+    if (!existingSeller) {
+      return errorResponse(res, 404, "Seller Not Found");
+    }
+    const seller = await Seller.findByIdAndUpdate(
+      existingSeller._id,
+      {
+        name,
+        contactDetails,
+        cityId,
+      },
+      { new: true },
+    );
+    return successResponse(res , 200, {message:"Seller Updated Successfully",seller })
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.deleteSeller = async (req, res, next) => {
+  try {
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.get = async (req, res, next) => {
+  try {
+  } catch (err) {
+    next(err);
+  }
+};
--- routes/v1/seller.js
+++ routes/v1/seller.js
@@ -1,13 +1,19 @@
-const express = require("express")
-const router = express.Router()
-const {auth}= require("./../../middlewares/auth")
-const roleGuard = require("./../../middlewares/roleGuard")
-const { create } = require("../../controllers/v1/seller")
+const express = require("express");
+const router = express.Router();
+const { auth } = require("./../../middlewares/auth");
+const roleGuard = require("./../../middlewares/roleGuard");
+const {
+  create,
+  update,
+  deleteSeller,
+  get,
+} = require("../../controllers/v1/seller");
 
+router
+  .route("/")
+  .post(auth, roleGuard("SELLER"), create)
+  .patch(auth, roleGuard("SELLER"), update)
+  .delete(auth, roleGuard("SELLER"), deleteSeller)
+  .get(auth, roleGuard("SELLER"), get);
 
-router.route("/").post(auth,roleGuard("SELLER"),create)
-
-
-
-
-module.exports = router 
+module.exports = router;
