565ea277ff5f391970ae67e7ff87eb4966dc1f29
Mohammad Naghavi Olyaei <mohammad.naghavi@gmail.com>
2026-06-17 12:37:51 +0330

    feat: add dynamic filters to categories

--- models/Category.js
+++ models/Category.js
@@ -26,12 +26,47 @@ const categorySchema = new mongoose.Schema({
         require: true,
         trim: true,
       },
-      path:{
+      path: {
         type: String,
         require: true,
         trim: true,
       },
     },
+  },
+  filters: {
+    type: [
+      {
+        name: {
+          type: String,
+          required: true,
+          trim: true,
+        },
+        slug: {
+          type: String,
+          required: true,
+          trim: true,
+          unique: true,
+        },
+        description: {
+          type: String,
+          trim: true,
+        },
+        type: {
+          type: String,
+          enum: ["radio", "selectbox"],
+          required: true,
+        },
+        Options: {
+          type: [String],
+          default:undefined,
+          validate: {
+            validator: (options) => Array.isArray(options),
+          },
+        },
+        min:{type:Number},
+        max:{type:Number},
+      },
+    ],
   },
 });
 
--- models/SubCategory.js
+++ models/SubCategory.js
@@ -20,6 +20,41 @@ const categorySchema = new mongoose.Schema({
     type: String,
     trim: true,
   },
+  filters: {
+    type: [
+      {
+        name: {
+          type: String,
+          required: true,
+          trim: true,
+        },
+        slug: {
+          type: String,
+          required: true,
+          trim: true,
+          unique: true,
+        },
+        description: {
+          type: String,
+          trim: true,
+        },
+        type: {
+          type: String,
+          enum: ["radio", "selectbox"],
+          required: true,
+        },
+        Options: {
+          type: [String],
+          default:undefined,
+          validate: {
+            validator: (options) => Array.isArray(options),
+          },
+        },
+        min:{type:Number},
+        max:{type:Number},
+      },
+    ],
+  },
   
 });
 
