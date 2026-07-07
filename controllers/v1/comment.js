166ec320864d10301a5e9da53cb0d515374d8a17
mohammad naghavi <mohammad.naghavi@gmail.com>
2026-07-07 13:32:17 +0330

    feat: implement comment creation by user

--- controllers/v1/comment.js
+++ controllers/v1/comment.js
@@ -1,3 +1,8 @@
+const Product = require("./../../models/Product");
+const Comment = require("./../../models/Comment");
+
+const { errorResponse, successResponse } = require("./../../helpers/responses");
+
 exports.getComments = async (req, res, next) => {
   try {
     //! Codes
@@ -8,7 +13,27 @@ exports.getComments = async (req, res, next) => {
 
 exports.createComment = async (req, res, next) => {
   try {
-    //! Codes
+    const user = req.user;
+    const { productId, rating, content } = req.body;
+
+    //TODO Validator
+    const product = await Product.findById(productId);
+
+    if (!product) {
+      return errorResponse(res, 404, "Product Not Found !!");
+    }
+
+    const newComment = await Comment.create({
+      product: productId,
+      user: user._id,
+      rating,
+      content,
+      replies: [],
+    });
+    return successResponse(res, 201, {
+      message: "Comment Created Successfully",
+      comment: newComment,
+    });
   } catch (err) {
     next(err);
   }
