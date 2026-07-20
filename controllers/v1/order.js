7e7b6e0a204ad9d7f07f91a20450fe76b9270954
Mohammad Naghavi Olyaei <mohammad.naghavi@gmail.com>
2026-07-20 11:15:12 +0330

    feat: fetch all orders by admin and order owner

--- controllers/v1/order.js
+++ controllers/v1/order.js
@@ -1,6 +1,30 @@
+const { successResponse } = require("../../helpers/responses");
+const Order = require("./../../models/Order");
+const { createPaginationData } = require("./../../utils/index");
+
 exports.getAllOrders = async (req, res, next) => {
   try {
-    //Codes
+    const { page = 1, limit = 10 } = req.query;
+    const user = req.user;
+
+    const filters = {
+      ...(user.roles.includes("ADMIN") ? {} : { user: user._id }),
+    };
+
+    const orders = await Order.find(filters)
+      .sort({ createdAt: "desc" })
+      .skip((page - 1) * limit)
+      .limit(limit)
+      .populate("user")
+      .populate("items.product")
+      .populate("items.seller");
+
+    const totalOrders = await Order.countDocuments(filters);
+
+    return successResponse(res, 200, {
+      orders,
+      pagination: createPaginationData(page, limit, totalOrders, "Orders"),
+    });
   } catch (err) {
     next(err);
   }
